62135553848969ac4065ff45db98cdff1473007a
Alexey Serezhenko <95300446+BipBopViy@users.noreply.github.com>
2022-04-04 23:51:11 +0300

    Сделал дизайн поиска билетов

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/common.blocks/main-header/declaration.css">
     <link rel="stylesheet" href="/common.blocks/section-popular/declaration.css">
     <link rel="stylesheet" href="/common.blocks/footer/declaration.css">
@@ -12,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Serezhenko TM</title>
 </head>
+
 <body>
     <div class="wrapper">
         <header class="main-header">
@@ -21,54 +23,94 @@
                     <span class="main-header__logo-text"><img class="main-header__img-logo" src="https://img.icons8.com/dotty/80/000000/high-speed-train.png" /></span>
                 </div>
                 <div class="main-header__account">
-                    <p class="main-header__acc">Личный кабинет</p>
+                    <a href="" class="main-header__acc">Личный кабинет</a>
                     <span class="main-header__logo-text"><img class="main-header__img-cab" src="/common.blocks/main-header/codicon_account.png"></span>
                 </div>
             </div>
-            
-            
+
+            <div class="grid-mother">
+                <form action="" method="post" >
+                    <div class="seldemo">
+                        <select name="selphp" class="border" value="ТЫ пидоп">
+                            <option value="" class="border">Откуда</option>
+                            <option value="" class="border">Воронеж</option>
+                            <option value="" class="border">Питер</option>
+                            <option value="" class="border">Красноадр</option>
+                            <option value="" class="border">Железногорск</option>
+                        </select>
+                    </div>
+                </form>
+
+                <form action="" method="post">
+                    <div class="seldemo">
+                        <select name="selphp" class="border">
+                            <option value="" class="border">Куда</option>
+                            <option value="" class="border">Воронеж</option>
+                            <option value="" class="border">Питер</option>
+                            <option value="" class="border">Красноадр</option>
+                            <option value="" class="border">Железногорск</option>
+                        </select>
+                    </div>
+                </form>
+
+                <form action="" method="post">
+                    <div class="seldemo">
+                        <input type="date" class="border">
+                    </div>
+                </form>
+
+                <form action="" method="post">
+                    <div class="seldemo">
+                        <input type="date" class="border">
+                    </div>
+                </form>
+
+                <input type="submit" value="Найти" class="btncls">
+
+            </div>
+
             <img src="/common.blocks/main-header/qwe2.jpg" class="main-header__bg">
         </header>
     </div>
-        <div class="section-1">
+    <div class="section-1">
         <h1 class="section-1__h1">Популярные направления:</h1>
-        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1400р</span></div>    
-        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1000р</span></div>   
-        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1600р</span></div>   
-        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1500р</span></div>   
-        </div>
+        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1400р</span></div>
+        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1000р</span></div>
+        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1600р</span></div>
+        <div class="section-1__favorite"><span class="section-1__p">Москва-Питер</span><span class="section-1__p_cost">от 1500р</span></div>
+    </div>
 
-        <div class="news">
+    <div class="news">
         <h1 class="news__h1">Новости</h1>
-          <div class="wrapper-news">
+        <div class="wrapper-news">
             <div class="first__news">
                 <p class="">Более 400 дополнительных поездов будут курсировать в майские праздники</p>
                 <p class="">Подробнее...</p>
             </div>
             <div class="first__news">
-                <p  class="">Движение скоростных поездов «Аллегро» между Россией и Финляндией приостанавливается с 28 марта</p>
+                <p class="">Движение скоростных поездов «Аллегро» между Россией и Финляндией приостанавливается с 28 марта</p>
             </div>
             <div class="first__news">
-              <p  class="">Железнодорожный тур к цветущим степям Калмыкии ждет путешественников в апреле</p>
-               </div> 
+                <p class="">Железнодорожный тур к цветущим степям Калмыкии ждет путешественников в апреле</p>
             </div>
         </div>
+    </div>
 
-        <div class="footer">
+    <div class="footer">
         <div class="footer__text-1">
             <div>8 800 775-00-00 звонок бесплатный для всех регионов РФ</div>
             <div>a.serezhenko@inbox.ru для вопросов, связанных с электронными билетами</div>
             <div class="main-header__TM-logo">
-              <span class="main-header__logo-text">Serezhenko TM</span>
-              <span class="main-header__logo-text"><img class="main-header__img-logo" src="https://img.icons8.com/dotty/80/000000/high-speed-train.png" /></span>
+                <span class="main-header__logo-text">Serezhenko TM</span>
+                <span class="main-header__logo-text"><img class="main-header__img-logo" src="https://img.icons8.com/dotty/80/000000/high-speed-train.png" /></span>
             </div>
-          </div>
-          <hr />
-          <div class="footer__text-2">
-           <div>© ОАО «РЖД», 2003 - 2022</div>
-           <div>Свидетельство о регистрации СМИ Эл.№ФС77-25927 При использовании любых материалов ссылка на rzd.ru обязательна</div>
-          </div>
         </div>
+        <hr />
+        <div class="footer__text-2">
+            <div>© ОАО «РЖД», 2003 - 2022</div>
+            <div>Свидетельство о регистрации СМИ Эл.№ФС77-25927 При использовании любых материалов ссылка на rzd.ru обязательна</div>
+        </div>
+    </div>
 </body>
 
 </html>
